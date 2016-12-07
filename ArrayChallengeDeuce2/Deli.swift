4000bd3971dace89b11cf07467c12a57254c8898
TJ <tjcarney@stny.rr.com>
2016-12-07 15:59:21 -0500

    Completed Lab

--- ArrayChallengeDeuce2/Deli.swift
+++ ArrayChallengeDeuce2/Deli.swift
@@ -15,25 +15,57 @@ class Deli {
     
     // 1
     func addNameToLine(name: String) -> String {
-                
-       // TODO: Implement this function.
+        var statement = ""
+        if name == "Billy Crystal" {
+            line.insert("Billy Crystal", at: 0)
+        } else if name == "Meg Ryan" {
+            line.insert("Meg Ryan", at: 0)
+        } else {
+            line.append("\(name)")
+        }
+        
+        for (index, name) in line.enumerated() {
+            if line[0] == "Billy Crystal" {
+                statement = "Welcome Billy Crystal! You can sit wherever you like."
+            } else if line[0] == "Meg Ryan" {
+                statement = "Welcome Meg Ryan! You can sit wherever you like."
+            } else if name == line[0] {
+                statement = "Welcome \(name), you're first in line!"
+            } else {
+                statement = "Welcome \(name), you're number \(index + 1) in line."
+            }
+        }; print(line)
+        return statement
         
     }
     
+
     
     // 2
     func nowServing() -> String {
-        
-        // TODO: Implement this function.
+        var serving = ""
+        if line.isEmpty {
+            serving = "There is no one to be served"
+        } else {
+            serving = "Now serving \(line[0])!"
+        }; line.remove(at: 0)
+        return serving
 
     }
     
     
     // 3
     func lineDescription() -> String {
+        var status = "The line is:"
+        if line.isEmpty {
+            status = "The line is currently empty."
+        } else {
+            for (index, line) in line.enumerated() {
+                status += "\n\(index + 1). \(line)"
+        }
         
-        // TODO: Implement this function.
-
-    }
+    };  print(status)
+        return status
     
+    }
 }
